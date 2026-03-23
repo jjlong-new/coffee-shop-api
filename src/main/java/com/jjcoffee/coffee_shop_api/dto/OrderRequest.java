0d49c01c2c8d705747ab7143455c487b1c5ebb15
Joseph Joven Long <joseph.joven.long@ibm.com>
2026-03-23 15:05:17 +0800

    OrderRequest revision

--- src/main/java/com/jjcoffee/coffee_shop_api/dto/OrderRequest.java
+++ src/main/java/com/jjcoffee/coffee_shop_api/dto/OrderRequest.java
@@ -21,6 +21,6 @@ public class OrderRequest {
 
     @NotNull(message = "Quantity is required")
     @Min(value = 1, message = "Quantity must be >= 1")
-    private int quantity;
+    private Integer quantity;
 
 }
